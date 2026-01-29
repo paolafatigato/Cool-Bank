549fc2ecb89aeefb462fa333623eff8f4baa0750
paolafatigato <127511067+paolafatigato@users.noreply.github.com>
2026-01-29 18:45:35 +0100

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+/* =============================================
+   Firebase Configuration
+   ============================================= */
+   
+const firebaseConfig = {
+  apiKey: "AIzaSyAFgLS8M7hiKs4gPyDBPCxRjU-6DAdE1-A",
+  authDomain: "schoolbank-4a39a.firebaseapp.com",
+  projectId: "schoolbank-4a39a",
+  storageBucket: "schoolbank-4a39a.firebasestorage.app",
+  messagingSenderId: "993060739453",
+  appId: "1:993060739453:web:1c39a9db0e531de5d15414"
+};
